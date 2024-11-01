d00ff2d56249f291312d3c8d8d9072657c766fa1
Yong-ga-ri <rlfgks97@gmail.com>
2024-11-02 03:16:55 +0900

    init: add basic dummy data

--- init/02-create-tables.sql
+++ init/02-create-tables.sql
@@ -1,8 +1,56 @@
 -- Create tables in the target database
 USE brewnet;
-CREATE TABLE IF NOT EXISTS member (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    description TEXT
+
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member (
+	member_code	INT	NOT NULL,
+	id	VARCHAR(255)	NOT NULL,
+	password	VARCHAR(255)	NOT NULL,
+	name	VARCHAR(255)	NOT NULL,
+	nickname	VARCHAR(255)	NOT NULL
 );
+
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE role (
+	role_code	INT	NOT NULL,
+	role	ENUM('마스터', '관리자', '본사 책임자', '본사 직원', '가맹점', '배송기사')	NOT NULL
+);
+
+DROP TABLE IF EXISTS member_role;
+
+CREATE TABLE member_role (
+	role_code	INT	NOT NULL,
+	member_code	INT	NOT NULL
+);
+
+ALTER TABLE member ADD CONSTRAINT PK_MEMBER PRIMARY KEY (
+	member_code
+);
+
+ALTER TABLE role ADD CONSTRAINT PK_ROLE PRIMARY KEY (
+	role_code
+);
+
+ALTER TABLE member_role ADD CONSTRAINT PK_MEMBER_ROLE PRIMARY KEY (
+	role_code,
+	member_code
+);
+
+ALTER TABLE member_role ADD CONSTRAINT FK_role_TO_member_role_1 FOREIGN KEY (
+	role_code
+)
+REFERENCES role (
+	role_code
+);
+
+ALTER TABLE member_role ADD CONSTRAINT FK_member_TO_member_role_1 FOREIGN KEY (
+	member_code
+)
+REFERENCES member (
+	member_code
+);
+
+ALTER TABLE member MODIFY member_code INT NOT NULL AUTO_INCREMENT;
+ALTER TABLE role MODIFY role_code INT NOT NULL AUTO_INCREMENT;
--- init/03-insert-dummy-data.sql
+++ init/03-insert-dummy-data.sql
@@ -1,3 +1,16 @@
 -- Insert dummy data into the example_table
 USE brewnet;
--- INSERT INTO member (name, password, description) VALUES ('Sample Name', 'Sample Description');
+INSERT INTO role (role) VALUES ('마스터');
+INSERT INTO role (role) VALUES ('관리자');
+INSERT INTO role (role) VALUES ('본사 책임자');
+INSERT INTO role (role) VALUES ('본사 직원');
+INSERT INTO role (role) VALUES ('가맹점');
+INSERT INTO role (role) VALUES ('배송기사');
+
+INSERT INTO member (id, password, name, nickname) VALUES ('master', 'masterpw', 'mastername', 'masternickname');
+
+-- 마스터 계정 생성
+INSERT INTO member_role (role_code, member_code)
+SELECT r.role_code, m.member_code
+FROM role r, member m
+WHERE r.role = '마스터' AND m.id = 'master';
